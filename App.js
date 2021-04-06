39048945f67eb3799c0c2dbaa25a514d453d0485
pratikbhosale535 <pratikbhosale535@gmail.com>
2021-04-06 14:05:53 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+ class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> App </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
